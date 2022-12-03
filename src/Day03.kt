ec29010570c600ae1fd2fbd9ec9f4dcf66ae4f1e
Walter Huf <hufman@gmail.com>
2022-12-02 21:33:55 -0800

    Day03b

--- src/Day03.kt
+++ src/Day03.kt
@@ -24,10 +24,24 @@ fun main() {
 		}.sum()
 	}
 
+	fun part2(input: List<String>): Int {
+		val troops = input.chunked(3)
+		val badges = troops.map { sacks ->
+			sacks.fold(sacks[0].toCharArray().toSet()) { acc, sack ->
+				acc.intersect(sack.toCharArray().toSet())
+			}.first()
+		}
+		return badges.map {
+			score(it)
+		}.sum()
+	}
+
 	// test if implementation meets criteria from the description, like:
 	val testInput = readInput("Day03_test")
 	check(part1(testInput) == 157)
+	check(part2(testInput) == 70)
 
 	val input = readInput("Day03")
 	println(part1(input))
+	println(part2(input))
 }
