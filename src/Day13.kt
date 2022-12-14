e8a5f7d021fbdfe9bd2f19904d768004b5425aae
Walter Huf <hufman@gmail.com>
2022-12-13 20:41:47 -0800

    Day13b

--- src/Day13.kt
+++ src/Day13.kt
@@ -2,16 +2,14 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.*
 
 fun main() {
-	fun parse(input: List<String>): List<Pair<JsonArray, JsonArray>> {
+	fun parse(input: List<String>): List<JsonArray> {
 		return input
 			.filter { it.isNotEmpty() }
-			.map { Json.decodeFromString<JsonArray>(it) }
-			.chunked(2) {
-				Pair(it[0], it[1])
-			}
+			.map { Json.decodeFromString(it) }
+
 	}
 
-	fun compare(left: JsonArray, right: JsonArray): Int {
+	fun comparePacket(left: JsonArray, right: JsonArray): Int {
 //		println("  - Comparing $left vs $right")
 		left.forEachIndexed { index, l ->
 			if (index >= right.size) {
@@ -24,11 +22,11 @@ fun main() {
 				l.int.compareTo(r.int)
 			} else {
 				if (l is JsonArray && r is JsonArray) {
-					compare(l, r)
+					comparePacket(l, r)
 				} else if (l is JsonPrimitive && r is JsonArray) {
-					compare(JsonArray(listOf(l)), r)
+					comparePacket(JsonArray(listOf(l)), r)
 				} else if (l is JsonArray && r is JsonPrimitive) {
-					compare(l, JsonArray(listOf(r)))
+					comparePacket(l, JsonArray(listOf(r)))
 				} else {
 					0
 				}
@@ -40,10 +38,13 @@ fun main() {
 	}
 
 	fun part1(input: List<String>): Int {
-		val pairs = parse(input)
-		return pairs
+		val lines = parse(input)
+		return lines
+			.chunked(2) {
+				Pair(it[0], it[1])
+			}
 			.foldIndexed(0) { index, acc, pair: Pair<JsonArray, JsonArray> ->
-				val correct = compare(pair.first, pair.second)
+				val correct = comparePacket(pair.first, pair.second)
 //				println("$correct ${pair.first} ${pair.second}")
 				if (correct == -1) {
 					acc + index + 1
@@ -52,11 +53,30 @@ fun main() {
 				}
 			}
 	}
+	
+	fun part2(input: List<String>): Int {
+		val first = JsonArray(listOf(JsonArray(listOf(JsonPrimitive(2)))))
+		val second = JsonArray(listOf(JsonArray(listOf(JsonPrimitive(6)))))
+		val lines = parse(input) +
+				listOf(first, second)
+		val sorted = lines.sortedWith(object: Comparator<JsonArray> {
+			override fun compare(p0: JsonArray?, p1: JsonArray?): Int {
+				p0 ?: return 0
+				p1 ?: return 0
+				return comparePacket(p0, p1)
+			}
+		})
+		val firstIndex = sorted.indexOf(first)
+		val secondIndex = sorted.indexOf(second)
+		return (firstIndex + 1) * (secondIndex + 1)
+	}
 
 	// test if implementation meets criteria from the description, like:
 	val testInput = readInput("Day13_test")
 	check(part1(testInput) == 13)
+	check(part2(testInput) == 140)
 
 	val input = readInput("Day13")
 	println(part1(input))
+	println(part2(input))
 }
